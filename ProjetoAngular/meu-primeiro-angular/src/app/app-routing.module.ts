c533e78c332c632b81fbb1da3f5c630e8b49aafa
oliveiramarcelo12 <oliveiramarcelo198@gmail.com>
2023-10-06 21:58:16 -0300

    06/10/2023

--- ProjetoAngular/meu-primeiro-angular/src/app/app-routing.module.ts
+++ ProjetoAngular/meu-primeiro-angular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ContatoComponent } from './contato/contato.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  { path: 'header', component: HeaderComponent },
+{ path: 'footer', component: FooterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
