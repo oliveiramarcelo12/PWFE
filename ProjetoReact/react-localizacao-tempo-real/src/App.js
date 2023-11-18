0954035f5f13943c2d3dad4e7b27c9f264d46cee
oliveiramarcelo12 <oliveiramarcelo198@gmail.com>
2023-11-17 21:35:41 -0300

    17/11/2023

---
+++ ProjetoReact/react-localizacao-tempo-real/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import MapaAtual from './Mapa/MapaAtual';
+import MapaLongitudeLatitude from './Mapa/MapaLongitudeLatitude';
+// Define o componente principal da aplicação, chamado 'App'.
+function App() {
+return (
+
+<div className="App">
+<MapaLongitudeLatitude />
+</div>
+);
+}
+// Exporta o componente 'App' como o componente principal da aplicação.
+export default App;
